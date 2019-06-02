fixed bugs in calculation. ODE now returns good response.

model/calculation.py
@@ -5,7 +5,7 @@ from model.helperFunctions import returnK
 
 
 def fun(x, y, g, m, d, rho, mu=0.0000157):
-    k = returnK(v = y[1], rho = rho, d = d, mu = mu, m = m)
+    k = returnK(v = y[1], rho = rho, d = d, mu = mu)
     #Vrnjen vektor
     return np.array([y[1], g-k/m*y[1]**2])
 
@@ -17,11 +17,11 @@ def solveODE(m, rho_air_value, y_points, diam, height, g0):
     tla.direction = -1
 
     #Numerični izračun rešitve
-    res = solve_ivp(lambda t, y: fun(t, y, m=m, d=diam, rho=float(rho_air_value/1000), g=g0), [y_points[0], y_points[-1]], y0=np.array([0,0]), t_eval=y_points,events=tla)
+    res = solve_ivp(lambda t, y: fun(t, y, m=m, d=diam, rho=float(rho_air_value), g=g0), [y_points[0], y_points[-1]], y0=np.array([0,0]), t_eval=y_points,events=tla)
     y,v=res.y
 
     #Izračun pospeška
-    a=fun(y_points,res.y,m=m, d=diam, rho=float(rho_air_value/1000),g=g0)[1]
+    a=fun(y_points,res.y,m=m, d=diam, rho=float(rho_air_value),g=g0)[1]
 
     return a,v,y, res.t
 

model/helperFunctions.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-def returnK(v, rho, d, mu, m):
+def returnK(v, rho, d, mu):
     # Konstante izračuna Cd ( https://www.researchgate.net/publication/244155878 )
     A=0.1806
     B=0.6459
@@ -11,11 +11,11 @@ def returnK(v, rho, d, mu, m):
     Re = d*v*rho/mu+0.000000001
     Cd = 24/Re*(1+A*Re**B)+C/(1+D/Re)
      #Izračun konstante
-    k = (((d/2)**2)*Cd*rho*v**2)/2
-    return 0.2
+    k = ((np.pi*(d/2)**2)*Cd*rho)/2
+    return k
 
 def returnMass(diam, rho):
-    return (4*np.pi*(diam/2)**2*rho)/3
+    return rho*(4/3)*np.pi*(diam/2)**3
 
 def returnRho(p):
     return ((28.96/1000)*p*10**5)/(8.3144598*(20+273.15)) 